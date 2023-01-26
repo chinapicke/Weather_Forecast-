ffc731cffde325c469c2753bf739bc370195ce5c
chinapicke <chinapickering@hotmail.co.uk>
2023-01-26 21:01:21 +0700

    function working to get user city input into loop that finds the city's forecast

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,25 +1,28 @@
+// Get the user input and put into cityName variable
+var cityName = $('#search-input').val()
 
-// Show current weather in London
-var APIKey = "5b045dfac16392eda5cca0b2562f708e";
+// When user put in town and clicks button, put the city into the link for API
+$('#search-button').on('click', function (e) {
+    e.preventDefault();
+    var cityName = $('#search-input').val()
+    var APIKey = "5b045dfac16392eda5cca0b2562f708e";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // Create CODE HERE to Log the queryURL
+        console.log(queryURL);
+        // Create CODE HERE to log the resulting object
+        console.log(response);
+        var tempCalc = response.main.temp - 273.15;
+        var city = response.name;
+        var wind = response.wind.speed;
+        var humidity = response.main.humidity;
+        $('#city').text('City: ' + city);
+        $('#temp').text('Temperature: ' + tempCalc.toFixed(2));
+        $('#wind').text('Wind: ' + wind);
+        $('#humidity').text('Humidity: ' + humidity);
 
-// Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,UnitedKingdom&appid=" + APIKey;
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    // Create CODE HERE to Log the queryURL
-    console.log(queryURL);
-    // Create CODE HERE to log the resulting object
-    console.log(response);
-    var tempCalc = response.main.temp - 273.15;
-  var city = response.name;
-  var wind= response.wind.speed;
-  var humidity = response.main.humidity;
-  $('#city').text('City: '+city);
-  $('#temp').text('Temperature: '+tempCalc.toFixed(2));
-  $('#wind').text('Wind: '+wind);
-  $('#humidity').text('Humidity: '+humidity);
-
-  })
+    })
+})
