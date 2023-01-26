eeead995cd5f93508d00bbc234e21c603311444d
chinapicke <chinapickering@hotmail.co.uk>
2023-01-26 22:47:11 +0700

    ammended some of the styling by removing classes and div from HTML

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -6,6 +6,7 @@ $('#search-button').on('click', function (e) {
     e.preventDefault()
     var cityName = $('#search-input').val()
     searchCity.push(cityName)
+    cityAPI()
     clearButtons()
     renderButtons()
     
@@ -15,6 +16,7 @@ function renderButtons(){
     for (var i = 0; i <searchCity.length; i++) {
         console.log(searchCity[i])
         var buttons = $('<button>')
+        buttons.attr({'id':"cityBtn", 'class': "col-sm-12"})
         buttons.text(searchCity[i])
         $("#history").append(buttons);
     }
@@ -28,31 +30,32 @@ function clearButtons(){
 var cityName = $('#search-input').val()
 
 // When user put in town and clicks button, put the city into the link for API
-// $('#search-button').on('click', function (e) {
-//     e.preventDefault();
-//     var cityName = $('#search-input').val()
-//     var APIKey = "5b045dfac16392eda5cca0b2562f708e";
-//     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response) {
-//         // Create CODE HERE to Log the queryURL
-//         console.log(queryURL);
-//         // Create CODE HERE to log the resulting object
-//         console.log(response);
-//         if (response == "False"){
-//             alert('Please input correct city name')
-//         }
-//         else{
-//         var tempCalc = response.main.temp - 273.15;
-//         var city = response.name;
-//         var wind = response.wind.speed;
-//         var humidity = response.main.humidity;
-//         $('#city').text('City: ' + city);
-//         $('#temp').text('Temperature: ' + tempCalc.toFixed(2));
-//         $('#wind').text('Wind: ' + wind);
-//         $('#humidity').text('Humidity: ' + humidity);
-//         }
-//     })
+function cityAPI(){
+    var cityName = $('#search-input').val()
+    var APIKey = "5b045dfac16392eda5cca0b2562f708e";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // Create CODE HERE to Log the queryURL
+        console.log(queryURL);
+        // Create CODE HERE to log the resulting object
+        console.log(response);
+        // if (response === 404){
+        //     alert('Please input correct city name')
+        // }
+        // else{
+        var tempCalc = response.main.temp - 273.15;
+        var city = response.name;
+        var wind = response.wind.speed;
+        var humidity = response.main.humidity;
+        $('#city').text('City: ' + city);
+        $('#temp').text('Temperature: ' + tempCalc.toFixed(2));
+        $('#wind').text('Wind: ' + wind);
+        $('#humidity').text('Humidity: ' + humidity);
+        }
+    // })
 // })
+    )
+}
